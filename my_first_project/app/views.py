73b8ae28965f72e6e9f2513b32bb4fbd280c4aeb
Slimkin <doc.slimkin.dgi@gmail.com>
2020-07-20 16:15:06 +0300

    add all

---
+++ my_first_project/app/views.py
@@ -0,0 +1,33 @@
+from django.http import HttpResponse
+from django.shortcuts import render, reverse
+from datetime import datetime, timezone
+import pytz
+import os
+
+def home_view(request):
+    template_name = 'app/home.html'
+    pages = {
+        'Главная страница': reverse('home'),
+        'Показать текущее время': reverse('time'),
+        'Показать содержимое рабочей директории': reverse('workdir')
+    }
+    
+    # context и параметры render менять не нужно
+    # подбробнее о них мы поговорим на следующих лекциях
+    context = {
+        'pages': pages
+    }
+    return render(request, template_name, context)
+
+
+def time_view(request):
+    current_time = datetime.now(pytz.timezone('Europe/Moscow'))
+    msg = f'Текущее время: {current_time}'
+    return HttpResponse(msg)
+
+
+def workdir_view(request):
+    workdir_data = os.listdir(path='.')
+    for f in workdir_data:
+        
+return HttpResponse(workdir_data)
